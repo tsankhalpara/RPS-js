6bf575306d7b2ec3cbfcb0cf8fb56b8d40ba89e8
Tara Sankhalpara <tsankhalpara@mac.com>
2019-09-11 19:03:23 +0100

    link js file to html file

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const userScore = 0;
 const computerScore = 0;
-const userScore_span = document.getElementById("user-score")
-const computerScore_span = document.getElementById("computer-score")
-const scoreboard_div = document.getElementsByClassName("score-board")
-const result_div = document.getElementsByClassName("result")
-const rock_div = document.getElementById("r")
-const paper_div = document.getElementById("p")
-const scissors_div = document.getElementById("s")
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
+const scoreboard_div = document.getElementsByClassName("score-board");
+const result_div = document.getElementsByClassName("result");
+const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const scissors_div = document.getElementById("s");
+
+rock_div.addEventListener('click', function() {
+  console.log("You clicked rock");
+})
